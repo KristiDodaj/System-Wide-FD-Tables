679eea253a982ccdca512cb3f2cb9e7b506941d8
KristiDodaj <kristidodaj001@gmail.com>
2023-03-02 22:41:07 -0500

    added new file

--- DataCollection.c
+++ DataCollection.c
@@ -42,62 +42,61 @@ void getProcesses()
         if (entry->d_type == DT_DIR && (pid = strtol(entry->d_name, &endPointer, 10)) > 0 && *endPointer == '\0')
         {
             // printf("PID: %ld\n", pid);
-        }
 
-        // enter the fd directory
-        char path[4096];
-        snprintf(path, 4096, "/proc/%s/fd", entry->d_name);
-        DIR *fd_directory = opendir(path);
+            // enter the fd directory
+            char path[4096];
+            snprintf(path, 4096, "/proc/%s/fd", entry->d_name);
+            DIR *fd_directory = opendir(path);
 
-        // when unsucesfully opened
-        if (fd_directory == NULL)
-        {
-            continue;
-        }
-
-        // go through fd files
-        struct dirent *fd_entry;
-        while ((fd_entry = readdir(fd_directory)) != NULL)
-        {
-            // skip over "." and ".."
-            if (strcmp(fd_entry->d_name, ".") == 0 || strcmp(fd_entry->d_name, "..") == 0)
+            // when unsucesfully opened
+            if (fd_directory == NULL)
             {
                 continue;
             }
-            // update the fd file path
-            int fd = atoi(fd_entry->d_name);
-            snprintf(path, 4096, "/proc/%s/fdinfo/%d", entry->d_name, fd);
-            FILE *fd_info = fopen(path, "r");
 
-            // continue if unsucessful in opening
-            if (fd_info == NULL)
+            // go through fd files
+            struct dirent *fd_entry;
+            while ((fd_entry = readdir(fd_directory)) != NULL)
             {
-
-                continue;
-            }
-
-            char *line = NULL;
-            size_t len = 0;
-            ssize_t nread;
-            while ((nread = getline(&line, &len, fd_info)) != -1)
-            {
-                if (strncmp(line, "pos:", 4) == 0)
+                // skip over "." and ".."
+                if (strcmp(fd_entry->d_name, ".") == 0 || strcmp(fd_entry->d_name, "..") == 0)
                 {
-                    char *pos = line + 4;
-                    while (*pos == ' ')
-                    {
-                        pos++;
-                    }
-                    printf("%-10s%-20d%-20s%s", entry->d_name, fd, fd_entry->d_name, pos);
-                    break;
+                    continue;
                 }
-            }
-            fclose(fd_info);
-            free(line);
-        }
-        closedir(fd_directory);
-    }
+                // update the fd file path
+                int fd = atoi(fd_entry->d_name);
+                snprintf(path, 4096, "/proc/%s/fdinfo/%d", entry->d_name, fd);
+                FILE *fd_info = fopen(path, "r");
 
+                // continue if unsucessful in opening
+                if (fd_info == NULL)
+                {
+
+                    continue;
+                }
+
+                char *line = NULL;
+                size_t len = 0;
+                ssize_t nread;
+                while ((nread = getline(&line, &len, fd_info)) != -1)
+                {
+                    if (strncmp(line, "pos:", 4) == 0)
+                    {
+                        char *pos = line + 4;
+                        while (*pos == ' ')
+                        {
+                            pos++;
+                        }
+                        printf("%-10s%-20d%-20s%s", entry->d_name, fd, fd_entry->d_name, pos);
+                        break;
+                    }
+                }
+                fclose(fd_info);
+                free(line);
+            }
+            closedir(fd_directory);
+        }
+    }
     closedir(directory);
 }
 
