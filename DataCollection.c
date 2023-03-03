ce1b7205d380da233369b7d0aad9130cc20bf0bf
KristiDodaj <kristidodaj001@gmail.com>
2023-03-02 22:34:55 -0500

    added new file

--- DataCollection.c
+++ DataCollection.c
@@ -1,7 +1,5 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include <stdio.h>
-#include <stdlib.h>
 #include <unistd.h>
 #include <dirent.h>
 #include <sys/types.h>
@@ -37,18 +35,73 @@ void getProcesses()
     struct dirent *entry;
     while ((entry = readdir(directory)) != NULL)
     {
+        long int pid;
+        char *endPointer;
 
         // validate that the entry is a directory that is a PID
-        if (atoi(entry->d_name) == 0)
+        if (entry->d_type == DT_DIR && (pid = strtol(entry->d_name, &endPointer, 10)) > 0 && *endPointer == '\0')
         {
-            printf("PID: %s\n", entry->d_name);
+            printf("PID: %ld\n", pid);
         }
+
+        // enter the fd directory
+        char path[4096];
+        snprintf(path, 4096, "/proc/%s/fd", entry->d_name);
+        DIR *fd_directory = opendir(path);
+
+        // when unsucesfully opened
+        if (fd_directory == NULL)
+        {
+            continue;
+        }
+
+        // go through fd files
+        struct dirent *fd_entry;
+        while ((fd_entry = readdir(fd_directory)) != NULL)
+        {
+            // skip over "." and ".."
+            if (strcmp(fd_entry->d_name, ".") == 0 || strcmp(fd_entry->d_name, "..") == 0)
+            {
+                continue;
+            }
+            // update the fd file path
+            int fd = atoi(fd_entry->d_name);
+            snprintf(path, 4096, "/proc/%s/fdinfo/%d", entry->d_name, fd);
+            FILE *fd_info = fopen(path, "r");
+
+            // continue if unsucessful in opening
+            if (fd_info == NULL)
+            {
+
+                continue;
+            }
+
+            char *line = NULL;
+            size_t len = 0;
+            ssize_t nread;
+            while ((nread = getline(&line, &len, fd_info)) != -1)
+            {
+                if (strncmp(line, "pos:", 4) == 0)
+                {
+                    char *pos = line + 4;
+                    while (*pos == ' ')
+                    {
+                        pos++;
+                    }
+                    printf("%-10s%-20d%-20s%s", entry->d_name, fd, fd_entry->d_name, pos);
+                    break;
+                }
+            }
+            fclose(fd_info);
+            free(line);
+        }
+        closedir(fd_directory);
     }
+
     closedir(directory);
 }
 
 int main()
 {
     getProcesses();
-    return 0;
 }
