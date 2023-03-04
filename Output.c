70b3b6346e713df2fb008daa2ed45d47cab14f1e
KristiDodaj <kristidodaj001@gmail.com>
2023-03-04 18:39:58 -0500

    added more files

--- Output.c
+++ Output.c
@@ -4,7 +4,7 @@
 
 void getCompositeTable(process **processes, size_t count, long int pid)
 {
-    // This function will take in the array process **processes along with its length size_t count. Additioanally, the function will recieve the long int pid which will be equal to -1 if
+    // This function will take in the array process **processes along with its length size_t count and create a composite table. Additioanally, the function will recieve the long int pid which will be equal to -1 if
     // we want to go through every process, or equal to a particular pid if we want to generate a composite table for a specific process.
     // Note: The composite table is composed of (PID, FD, Filename, Inode) info
 
@@ -13,15 +13,34 @@ void getCompositeTable(process **processes, size_t count, long int pid)
     printf("%-6s==============================================================\n", " ");
 
     // print content
+    int counter = 0;
+    char str_pid[4096];
+    sprintf(str_pid, "%ld", pid);
+
     for (size_t i = 0; i < count; i++)
     {
         if (pid == -1)
         {
             printf("%-6ld%-10ld%-10ld%-20s\t\t%-10ld\n", i, (*processes + i)->pid, (*processes + i)->fd, (*processes + i)->filename, (*processes + i)->inode);
         }
+        else
+        {
+            if (strcmp((*processes + i)->pid, str_pid) == 0)
+            {
+                printf("%-6ld%-10ld%-10ld%-20s\t\t%-10ld\n", counter, (*processes + i)->pid, (*processes + i)->fd, (*processes + i)->filename, (*processes + i)->inode);
+                counter++;
+            }
+        }
     }
 
     printf("%-6s==============================================================\n", " ");
+}
+
+void getPerProcess(process **processes, size_t count, long int pid)
+{
+    // This function will take in the array process **processes along with its length size_t count and create a per process table. Additioanally, the function will recieve the long int pid which will be equal to -1 if
+    // we want to go through every process, or equal to a particular pid if we want to generate a per process table for a specific process.
+    // Note: The per process table is composed of (PID, FD) info
 }
 
 int main()
