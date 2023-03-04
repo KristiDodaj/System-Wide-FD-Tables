392421fa5f024cd93ba070d3ab66a11ca63bc521
KristiDodaj <kristidodaj001@gmail.com>
2023-03-04 18:10:00 -0500

    added more files

--- Output.c
+++ Output.c
@@ -6,16 +6,17 @@ void getCompositeTable(process **processes, size_t count, long int pid)
 {
     // This function will take in the array process **processes along with its length size_t count. Additioanally, the function will recieve the long int pid which will be equal to -1 if
     // we want to go through every process, or equal to a particular pid if we want to generate a composite table for a specific process.
+    // Note: The composite table is composed of (PID, FD, Filename, Inode) info
 
     // print header
-    printf("PID\tFD\tFilename\t\tInode\n");
+    printf("%-10s%-10s%-10s%-10s\n", "PID", "FD", "Filename", "Inode");
     printf("===============================================\n");
 
     for (size_t i = 0; i < count; i++)
     {
         if (pid == -1)
         {
-            printf("%ld\t%ld\t%s\t\t%lu\n", (*processes + i)->pid, (*processes + i)->fd, (*processes + i)->filename, (*processes + i)->inode);
+            printf("%-10ld%-10ld%-10s%-10ld\n", (*processes + i)->pid, (*processes + i)->fd, (*processes + i)->filename, (*processes + i)->inode);
         }
     }
 }
