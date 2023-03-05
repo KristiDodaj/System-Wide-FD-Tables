3ea18f3f5e82c239e761964754ca79fa88af5a11
KristiDodaj <kristidodaj001@gmail.com>
2023-03-04 19:12:04 -0500

    added more files

--- Output.c
+++ Output.c
@@ -36,13 +36,13 @@ void getCompositeTable(process **processes, size_t count, long int pid)
     {
         if (pid == -1)
         {
-            printf("%-6s%-10ld%-10ld%-20s%-10ld\n", " ", (*processes + i)->pid, (*processes + i)->fd, (*processes + i)->filename, (*processes + i)->inode);
+            printf("%-6s%-10ld%-10ld%-30s%-10ld\n", " ", (*processes + i)->pid, (*processes + i)->fd, (*processes + i)->filename, (*processes + i)->inode);
         }
         else
         {
             if ((*processes + i)->pid == pid)
             {
-                printf("%-6s%-10ld%-10ld%-20s%-10ld\n", " ", (*processes + i)->pid, (*processes + i)->fd, (*processes + i)->filename, (*processes + i)->inode);
+                printf("%-6s%-10ld%-10ld%-30s%-10ld\n", " ", (*processes + i)->pid, (*processes + i)->fd, (*processes + i)->filename, (*processes + i)->inode);
                 counter++;
             }
         }
