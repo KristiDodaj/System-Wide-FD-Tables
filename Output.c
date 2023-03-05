ef49fe9792d03cf53ddbbbb9047f947e4a265071
KristiDodaj <kristidodaj001@gmail.com>
2023-03-05 02:05:55 -0500

    added more files

--- Output.c
+++ Output.c
@@ -320,5 +320,5 @@ int main()
 {
     process **processes = (process **)malloc(sizeof(process *));
     size_t count = getProcesses(processes);
-    outputText(processes, count, -1);
+    outputBinary(processes, count, -1);
 }
